5cf2f841fdc392aa1c6de58163489e13cc2f648e
NlaakStudios <andrew@nlaak.com>
2018-07-04 00:16:41 -0500

    - Updated company name in headers - Changed from private to MIT license - New default example html document - updated to Bootstrap 4.1.1

--- src/js/autojigger.js
+++ src/js/autojigger.js
@@ -5,11 +5,11 @@
 
 /*!
  *  Nlaak Studios Auto-Jigger @andrewdonelson - https://www.nlaak.com/#autojigger - @nlaakstudios
- *  License - Apache 2.0
+ *  License -  MIT
  */
 /*! Auto Jigger Core Javascript */
 AJ$ = (function () {
-	
+
 	/**
 	 * Shorthand version of window
 	 *
@@ -17,8 +17,8 @@ AJ$ = (function () {
 	 * @property W$
 	 * @type {window}
 	 * @memberof window
-	 */			
-	window['W$']=window;
+	 */
+	window['W$'] = window;
 
 	/**
 	 * Shorthand access to window.document
@@ -27,8 +27,8 @@ AJ$ = (function () {
 	 * @property D$
 	 * @type {document}
 	 * @memberof window
-	 */			
-	W$['D$']=W$.document;
+	 */
+	W$['D$'] = W$.document;
 
 	/**
 	 * Shorthand access to window.document.head[]
@@ -37,8 +37,8 @@ AJ$ = (function () {
 	 * @property H$
 	 * @type {DOMElement}
 	 * @memberof window
-	 */			
-	W$['H$']=D$.getElementsByTagName("head")[0];
+	 */
+	W$['H$'] = D$.getElementsByTagName("head")[0];
 
 	/**
 	 * Shorthand access to window.document.tail[]
@@ -47,8 +47,8 @@ AJ$ = (function () {
 	 * @property T$
 	 * @type {DOMElement}
 	 * @memberof window
-	 */			
-	W$['T$']=D$.getElementById("tail");
+	 */
+	W$['T$'] = D$.getElementById("tail");
 
 	/**
 	 * Shorthand access to window.document.body[]
@@ -57,8 +57,8 @@ AJ$ = (function () {
 	 * @property B$
 	 * @type {DOMElement}
 	 * @memberof window
-	 */			
-	W$['B$']=D$.getElementsByTagName("body")[0];
+	 */
+	W$['B$'] = D$.getElementsByTagName("body")[0];
 
 	/**
 	 * Shorthand access to window.document.stylesheets
@@ -67,8 +67,8 @@ AJ$ = (function () {
 	 * @property C$
 	 * @type {DOMEElement}
 	 * @memberof window
-	 */			
-	W$['C$']=D$.styleSheets;
+	 */
+	W$['C$'] = D$.styleSheets;
 
 	/**
 	 * Shorthand access to window.document.scripts
@@ -77,22 +77,22 @@ AJ$ = (function () {
 	 * @property S$
 	 * @type {DOMElement}
 	 * @memberof window
-	 */			
-	W$['S$']=D$.scripts;
-	
+	 */
+	W$['S$'] = D$.scripts;
+
 	var AutoJigger = {
-		VERSION : '3.3.0',
-		LOADING : 0,
-		LOADED 	: 0,
-		FILES 	: [],
-		READY	: false,
-						
+		VERSION: '3.3.0',
+		LOADING: 0,
+		LOADED: 0,
+		FILES: [],
+		READY: false,
+
 		/**
 		 * Utility function for loading JavaScript or Stylesheets.
 		 * No other file formats are supported.
 		 *
 		 * @method lds
-		 * @param {string} $fileType 	either script or link
+		 * @param {string} $filetype 	either script (js) or link (css)
 		 * @param {string} $id 			element ID of this file
 		 * @param {string} $url 		full path to file, local or remote
 		 * @param {boolean} $tail		set to true if you want script loaded into tail.
@@ -102,35 +102,35 @@ AJ$ = (function () {
 		 * @memberof AutoJigger
 		 * @export
 		 */
-		lds : function ($filetype, $id, $url, $tail, $after, $onLoad, $onError) {
-			
+		lds: function ($filetype, $id, $url, $tail, $after, $onLoad, $onError) {
+
 			function _onLoaded() {
-				console.log('File load successful.');			
+				console.log('File load successful.');
 			}
 
 			function _onErrored() {
 				console.log('File load failed.');
 			}
-			
+
 			function insert() {
 				//tail or head?
-				var c = ($tail===true)?T$:H$;
-				
-				if (typeof $after!='string') $after='last';
-				
-				if ($after=='first'){
+				var c = ($tail === true) ? T$ : H$;
+
+				if (typeof $after != 'string') $after = 'last';
+
+				if ($after == 'first') {
 					//=-=|Insert before all other scripts|=-=//
 					c.firstElementChild(a);
 					return true;
-				} else if ($after=='last'){
+				} else if ($after == 'last') {
 					//=-=|Insert before all other scripts|=-=//
 					c.appendChild(a);
 					return true;
 				} else {
 					//=-=|find $after and insert after|=-=//
-					c=gei($after);
-					if (typeof c !='undefined' && c !=null){
-						if (c.nextElementSibling!=null) {
+					c = gei($after);
+					if (typeof c != 'undefined' && c != null) {
+						if (c.nextElementSibling != null) {
 							c.nextElementSibling.insertBefore(a);
 							return true;
 						}
@@ -138,39 +138,39 @@ AJ$ = (function () {
 				}
 				return false;
 			}
-		
-			var a,b,c,d;
-					
+
+			var a, b, c, d;
+
 			//=-=|Prepare required parameters|=-=//
-			for (var i = 0;i<3;i++) {
-				arguments[i]=chk(arguments[i],false);
-				if (arguments[i]===false) {
+			for (var i = 0; i < 3; i++) {
+				arguments[i] = chk(arguments[i], false);
+				if (arguments[i] === false) {
 					if (console) console.log('Error');
 				}
 			}
 
-			if (arguments[0] == 'script'||arguments[0] == 'js') {
+			if (arguments[0] == 'script' || arguments[0] == 'js') {
 				a = D$.createElement('script');
 				a.async = 0;
-				a.src = $url; 
+				a.src = $url;
 				a.type = 'application/javascript';
-			} else if (arguments[0] == 'link'||arguments[0] == 'css') {
-				a= D$.createElement('link');
+			} else if (arguments[0] == 'link' || arguments[0] == 'css') {
+				a = D$.createElement('link');
 				a.href = $url;
 				a.rel = 'stylesheet';
 				a.type = 'text/css';
 			} else if (arguments[0] == 'html') {
 				//Define path to html to include
-				d=location.pathname + 'html/' + gfn($url) + '.inc' + gex($url);
+				d = location.pathname + 'html/' + gfn($url) + '.inc' + gex($url);
 				return true;
 			} else {
 				return false;
 			}
-			
-			a.id=arguments[1];
+
+			a.id = arguments[1];
 			a.onload = ($onLoad) ? $onLoad : _onLoaded.bind(this);
-			a.onerror = ($onError)? $onError : _onErrored.bind(this);
-			
+			a.onerror = ($onError) ? $onError : _onErrored.bind(this);
+
 			return insert();
 		},
 
@@ -181,32 +181,32 @@ AJ$ = (function () {
 		 * @param {Array} files	all scripts to be loaded.
 		 * @memberof AutoJigger
 		 * @export
-		 */			
-		las : function(filelist) {
-			
-			if(T$&&T$.tagName=="DIV"){
-				if(filelist&&filelist.constructor.name==="Array"){
-					var fn,tail,idx;
-					AJ$.LOADING=filelist.length;
-					AJ$.LOADED=0;
-					cls('logo','add','appear');
-					cls('wrapper','add','vanish','');
-					do{
-						fn=filelist.shift();
-						idx=AJ$['FILES'].indexOf(fn);
-						if (idx===-1) {
+		 */
+		las: function (filelist) {
+
+			if (T$ && T$.tagName == "DIV") {
+				if (filelist && filelist.constructor.name === "Array") {
+					var fn, tail, idx;
+					AJ$.LOADING = filelist.length;
+					AJ$.LOADED = 0;
+					cls('logo', 'add', 'appear');
+					cls('wrapper', 'add', 'vanish', '');
+					do {
+						fn = filelist.shift();
+						idx = AJ$['FILES'].indexOf(fn);
+						if (idx === -1) {
 							lds(
 								gex(fn),
-								gfn(fn), 
-								fn, 
-								((gex(fn)=='js')?true:false),
+								gfn(fn),
+								fn,
+								((gex(fn) == 'js') ? true : false),
 								'last',
-								AJ$.onFileLoaded, 
+								AJ$.onFileLoaded,
 								null
 							);
 							AJ$['FILES'].push(fn);
 						}
-					}while(filelist.length>0);					
+					} while (filelist.length > 0);
 				}
 			}
 		},
@@ -223,8 +223,10 @@ AJ$ = (function () {
 		 * @memberof AutoJigger
 		 * @export
 		 */
-		vld : function(a){return!(null===a||"undefined"===typeof a||!1===a||""===a)},
-		
+		vld: function (a) {
+			return !(null === a || "undefined" === typeof a || !1 === a || "" === a)
+		},
+
 		/**
 		 *  Checks if a param is valid (null or undefined) in which case the default value will be returned
 		 *
@@ -239,7 +241,9 @@ AJ$ = (function () {
 		 * @todo Remove function
 		 * @export
 		 */
-		chk : function($p,$def){return (!this['vld']($p))?$def:$p},
+		chk: function ($p, $def) {
+			return (!this['vld']($p)) ? $def : $p
+		},
 
 		/**
 		 * Alias for window.document.getElementById
@@ -253,7 +257,7 @@ AJ$ = (function () {
 		 * @memberof AutoJigger
 		 * @export
 		 */
-		gei : function ($p) {
+		gei: function ($p) {
 			return D$.getElementById($p);
 		},
 
@@ -269,44 +273,49 @@ AJ$ = (function () {
 		 * @memberof AutoJigger
 		 * @export
 		 */
-		cls : function($el,$action,$c1,$c2) {
+		cls: function ($el, $action, $c1, $c2) {
 			try {
-				$el=(typeof $el != 'string')
-				?
-				$el
-				:
-				gei($el);
-				
+				$el = (typeof $el != 'string') ?
+					$el :
+					gei($el);
+
 				if (!$el) return;
 				switch ($action.toLowerCase()) {
-					case 'set': 
-						$el.className=$c1;
+					case 'set':
+						$el.className = $c1;
 						break;
-						
-					case 'clear': 
-						$el.className='';
+
+					case 'clear':
+						$el.className = '';
 						break;
-						
-					case 'add': 
+
+					case 'add':
 						$el.classList.add($c1);
 						break;
-						
+
 					case 'remove':
-						try{$el.classList.remove($c1)}catch(e){}
+						try {
+							$el.classList.remove($c1)
+						} catch (e) {}
 						break;
-						
-					case 'replace': 
-						try{$el.classList.remove($c1)}catch(e){}
-						try{$el.classList.add($c2)}catch(e){}
+
+					case 'replace':
+						try {
+							$el.classList.remove($c1)
+						} catch (e) {}
+						try {
+							$el.classList.add($c2)
+						} catch (e) {}
 						break;
-						
+
 					case 'toggle':
 						$el.classList.toggle($c1);
 						break;
 				}
-			} catch(e) {
+			} catch (e) {
 				alert(e);
 			}
+
 		},
 
 		/**
@@ -318,10 +327,10 @@ AJ$ = (function () {
 		 * @memberof AutoJigger
 		 * @export 
 		 */
-		gex : function(a){
-			if (typeof a==='undefined'||a=='') return ''; else return /(?:\.([^.]+))?$/.exec(a)[1].toLowerCase();
+		gex: function (a) {
+			if (typeof a === 'undefined' || a == '') return '';
+			else return /(?:\.([^.]+))?$/.exec(a)[1].toLowerCase();
 		},
-
 		/**
 		 * Uses native javascript XMLHttpRequest GET
 		 *
@@ -343,11 +352,11 @@ AJ$ = (function () {
 		 * @memberof AutoJigger
 		 * @export 
 		 */
-		get : function(url, callback) {
+		get: function (url, callback) {
 			var xhr = new XMLHttpRequest();
 			xhr.open('GET', url, true);
 			xhr.responseType = 'json';
-			xhr.onload = function() {
+			xhr.onload = function () {
 				var status = xhr.status;
 				if (status === 200) {
 					callback(null, xhr.response);
@@ -358,10 +367,10 @@ AJ$ = (function () {
 			xhr.send();
 		},
 
-		put : function(url, callback) {
+		put: function (url, callback) {
 			var xhr = new XMLHttpRequest();
 			xhr.open("POST", url, true);
-			xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+			xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 			xhr.onload = function () {
 				var data = JSON.parse(xhr.responseText);
 				if (xhr.readyState == 4 && xhr.status == "201") {
@@ -382,63 +391,63 @@ AJ$ = (function () {
 		 * @memberof AutoJigger
 		 * @export 
 		 */
-		gfn : function(a){
-			if (typeof a==='string')return a.substring(a.lastIndexOf("/")+1);
+		gfn: function (a) {
+			if (typeof a === 'string') return a.substring(a.lastIndexOf("/") + 1);
 		},
 
 		/**[Events - internal]**/
-		onFileLoaded : function(e) {
+		onFileLoaded: function (e) {
 			AJ$.LOADED++;
-			if (AJ$.LOADED===AJ$.LOADING) {
+			if (AJ$.LOADED === AJ$.LOADING) {
 				//AutoJigger.LOADING=0;
 				if (!AJ$['READY']) {
-					if (!AJ$['READY'] && typeof(window['AutoJiggerReady']) == "function") {
+					if (!AJ$['READY'] && typeof (window['AutoJiggerReady']) == "function") {
 						window['AutoJiggerReady']();
-						AJ$['READY']=true;
+						AJ$['READY'] = true;
 					}
 				} else {
-					AJ$['READY']=true;
+					AJ$['READY'] = true;
 				}
 			}
-			
+
 			if (AJ$['READY']) {
-				cls('logo','replace','appear','vanish');
-				cls('wrapper','replace','vanish','appear');
+				cls('logo', 'replace', 'appear', 'vanish');
+				cls('wrapper', 'replace', 'vanish', 'appear');
 			}
 		}
 	};
-	
+
 	//temp fix until I rewrite the closure
 	AutoJigger['FILES'] = [];
 	AutoJigger['READY'] = false;
-	
+
 	/**[Exports - Not the Object itselt, just the public functions]**/
-	W$['vld'] = AutoJigger.vld;	
-	W$['chk'] = AutoJigger.chk;	
-	W$['gei'] = AutoJigger.gei;	
-	W$['cls'] = AutoJigger.cls;	
+	W$['vld'] = AutoJigger.vld;
+	W$['chk'] = AutoJigger.chk;
+	W$['gei'] = AutoJigger.gei;
+	W$['cls'] = AutoJigger.cls;
 	W$['gex'] = AutoJigger.gex;
-	W$['gfn'] = AutoJigger.gfn;	
-	W$['lds'] = AutoJigger.lds;	
-	W$['las'] = AutoJigger.las;	
-	
+	W$['gfn'] = AutoJigger.gfn;
+	W$['lds'] = AutoJigger.lds;
+	W$['las'] = AutoJigger.las;
+
 	/**[Start]**/
 	W$['lds'](
-		'js', 
-		'jquery', 
-		'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js', 
-		true, 
-		'last', 
-		function(){
+		'js',
+		'jquery',
+		'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+		true,
+		'last',
+		function () {
 			W$['las']([
-				'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css',
+				'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css',
 				'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js',
 				'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-				'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js'
+				'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.bundle.min.js'
 			]);
-		}, 
+		},
 		null
-	);		
-	
+	);
+
 	return AutoJigger;
 })();
